0b43142c988cf94f5bd0dce11fb626ed46c31db8
Siva Shankar <sivashankarcrr@gmail.com>
2018-12-14 14:02:28 +0530

    Adding new items to the git repo

--- JavaScript/Assignments/Bouncing Balls/main.js
+++ JavaScript/Assignments/Bouncing Balls/main.js
@@ -12,3 +12,104 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+function Ball(x,y, velX,velY, color,size)
+{
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.size = size;
+  this.color = color;
+
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+var balls = [];
+
+Ball.prototype.collisionDetect = function() {
+
+  var vanishedBallsIndices = [];
+  for (var j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        balls[j].size = balls[j].size-0.5;
+        if(balls[j].size < 4)
+        { vanishedBallsIndices.push(j);}
+      }
+    }
+  }
+
+  vanishedBallsIndices.reverse();
+  for (k=0;k<vanishedBallsIndices.length;k++)
+  {
+  balls.splice(vanishedBallsIndices[k],1);}
+}
+
+while (balls.length < 25) {
+  var size = random(10,20);
+  var ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+  //ctx.fillStyle = 'rgba(0, 0, 1, 0.25)';
+  //ctx.fillRect(height-10, height -10, width, height);
+
+  
+
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+
+//function calls
+loop();
